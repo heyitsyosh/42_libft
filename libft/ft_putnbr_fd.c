8a132a344cda0ae7adeb6a624f54b25d2fa7ead6
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2022-06-22 04:36:59 +0900

    update

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,11 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:29:00 by myoshika          #+#    #+#             */
-/*   Updated: 2022/06/15 16:02:15 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/06/22 00:22:14 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// void	ft_putnbr_fd(int n, int fd)
+// {
+	
+// }
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,13 +6,32 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:20:43 by myoshika          #+#    #+#             */
-/*   Updated: 2022/06/20 07:08:19 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/06/22 03:20:04 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-// char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
-// {
-	
-// }
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	needle_len;
+	char	*c_haystack;
+
+	if (needle == NULL)
+		return (NULL);
+	needle_len = ft_strlen(needle);
+	c_haystack = (char *)haystack;
+	if (*needle == '\0')
+		return ((char *)haystack);
+	if (len < needle_len)
+		return (NULL);
+	len -= needle_len - 1;
+	while (*c_haystack != '\0' && len--)
+	{
+		if (!ft_strncmp(c_haystack, needle, needle_len))
+			return (c_haystack);
+		c_haystack++;
+	}
+	return (NULL);
+}
