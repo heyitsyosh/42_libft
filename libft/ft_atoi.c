916051f3adef843d628dbf90e083aa40231a6c8a
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2022-06-24 01:56:08 +0900

    update

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,13 +6,46 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:20:06 by myoshika          #+#    #+#             */
-/*   Updated: 2022/06/21 14:33:14 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/06/24 01:54:08 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// int	ft_atoi(const char *str)
-// {
-	
-// }
+int	is_space(char c)
+{
+	if (c == 32 || (c <= 13 && c >= 9))
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	int		sign;
+	long	nbr;
+
+	sign = 1;
+	nbr = 0;
+	i = 0;
+	while (is_space(*(str + i)))
+		i++;
+	if ((*(str + i) == '+' || *(str + i) == '-') && *(str + i))
+	{
+		if (*(str + i) == '-')
+			sign = -1;
+		i++;
+	}
+	while (*(str + i) && ((*(str + i) >= '0') && (*(str + i) <= '9')))
+	{
+		if (nbr > INT_MAX / 10
+			|| (sign == 1 && (nbr * 10) + sign * (*(str + i) - '0') < 0))
+			return (-1);
+		else if (nbr < INT_MIN / 10
+			|| (sign == -1 && (nbr * 10) + sign * (*(str + i) - '0') > 0))
+			return (0);
+		nbr = (nbr * 10) + sign * (*(str + i) - '0');
+		i++;
+	}
+	return ((int)nbr);
+}
