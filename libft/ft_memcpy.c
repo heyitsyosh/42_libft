d1206c975848cf082391b477c139c57c589ddefa
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2022-07-14 00:09:04 +0900

    update

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:20:25 by myoshika          #+#    #+#             */
-/*   Updated: 2022/07/14 00:07:52 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/07/14 00:09:01 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char	*uc_src;
 	size_t			i;
 
-	// if (dst == NULL && src == NULL)
-	// 	return (NULL);
+	if (dst == NULL || src == NULL)
+		return (NULL);
 	uc_dst = dst;
 	uc_src = (unsigned char *)src;
 	i = 0;
