3d86d2f8dd1e3d739c785a5aca088f5181cb52be
maria <winterzaq1@gmail.com>
2022-07-13 22:00:45 +0900

    update

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:20:06 by myoshika          #+#    #+#             */
-/*   Updated: 2022/07/13 21:07:45 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/07/13 21:59:41 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,12 @@ static long	make_l(const char *str, int i, int sign, long num)
 {
 	while (*(str + i) && ((*(str + i) >= '0') && (*(str + i) <= '9')))
 	{
-		if (num < INT_MIN / 10
-			|| (sign == -1 && (num * 10) + sign * (*(str + i) - '0') > 0)
+		if (num * 10 > LONG_MAX / 10
 			|| (sign == 1 && (num * 10) + sign * (*(str + i) - '0') < 0))
-			return (0);
+			return (LONG_MAX);
+		else if (num * 10 < LONG_MIN / 10
+			|| (sign == -1 && (num * 10) + sign * (*(str + i) - '0') > 0))
+			return (LONG_MIN);
 		num = (num * 10) + sign * (*(str + i) - '0');
 		i++;
 	}
