aba9aa747b2da609347e8eba08500b6a5511c5e3
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2022-07-14 02:12:14 +0900

    update

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:20:43 by myoshika          #+#    #+#             */
-/*   Updated: 2022/07/14 02:10:53 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/07/14 02:12:09 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	{
 		if (!ft_strncmp(haystack, needle, needle_len))
 			return (c_haystack);
-		c_haystack++;
+		haystack++;
 	}
 	return (NULL);
 }
