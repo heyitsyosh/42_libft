b274f5a99b7eb93e158b3e6e80f4596793f0c9b9
maria <winterzaq1@gmail.com>
2022-07-14 01:57:41 +0900

    update

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:20:35 by myoshika          #+#    #+#             */
-/*   Updated: 2022/07/14 01:56:55 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/07/14 01:57:37 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	dst_len = ft_strlen(dst);
 	dst += dst_len;	
 	if (dstsize == 0)
-		return (src_len);
+		return (ft_strlen(src));
 	if (dst_len >= dstsize)
 		return (dstsize + ft_strlen(src));
 	ft_strlcpy(dst, src, dstsize - dst_len);
