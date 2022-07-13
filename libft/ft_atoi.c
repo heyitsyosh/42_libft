4b19c4bd8f68f6aeb5b39fc0b82db7224f8fd6ee
maria <winterzaq1@gmail.com>
2022-07-14 01:13:33 +0900

    update

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:20:06 by myoshika          #+#    #+#             */
-/*   Updated: 2022/07/14 00:54:51 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/07/14 01:13:23 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ static int	is_space(char c)
 	return (0);
 }
 
-static long	long make_l(const char *str, int i, int sign, long long num)
+static long	long	make_l(const char *str, int i, int sign, long long num)
 {
 	while (*(str + i) && ((*(str + i) >= '0') && (*(str + i) <= '9')))
 	{
-		if (sign == 1 && ((num > LONG_MAX / 10)
-				|| (num == LONG_MAX / 10 && *(str + i) > LONG_MAX % 10)))
+		if (sign == 1 && ((num > LONG_MAX / 10))
+				//|| (num == LONG_MAX / 10 && *(str + i) > LONG_MAX % 10)))
 			return (LONG_MAX);
-		else if (sign == -1 && ((num < LONG_MIN / 10)
-				|| (num == LONG_MIN / 10 && *(str + i) > LONG_MIN % 10)))
+		else if (sign == -1 && ((num < LONG_MIN / 10))
+				//|| (num == LONG_MIN / 10 && *(str + i) > LONG_MIN % 10)))
 			return (LONG_MIN);
 		num = (num * 10) + sign * (*(str + i) - '0');
 		i++;
@@ -37,8 +37,8 @@ static long	long make_l(const char *str, int i, int sign, long long num)
 
 int	ft_atoi(const char *str)
 {
-	int		i;
-	int		sign;
+	int			i;
+	int			sign;
 	long long	num;
 
 	sign = 1;
