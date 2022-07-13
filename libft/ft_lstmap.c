36bf111516d925b6a5ada4e2b8006b67b4b41ff6
maria <winterzaq1@gmail.com>
2022-07-13 21:08:11 +0900

    update

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmapi.c                                       :+:      :+:    :+:   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:28:44 by myoshika          #+#    #+#             */
-/*   Updated: 2022/07/11 01:11:39 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/07/13 20:39:15 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*tmp;
 	t_list	*head;
 
-	if (lst == NULL || f == NULL) //to null guard or to not null guard?? for the del and parameter function
+	if (lst == NULL || f == NULL)
 		return (NULL);
 	head = NULL;
 	while (lst != NULL)
