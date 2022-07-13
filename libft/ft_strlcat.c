9a39ac819566c4f0fd836b4bc34a194670458007
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2022-07-14 01:59:01 +0900

    update

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,22 +6,38 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:20:35 by myoshika          #+#    #+#             */
-/*   Updated: 2022/07/14 01:57:37 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/07/14 01:58:59 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+// size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+// {
+// 	size_t	dst_len;
+
+// 	dst_len = ft_strlen(dst);
+// 	dst += dst_len;
+// 	if (dst_len >= dstsize)
+// 		return (dstsize + ft_strlen(src));
+// 	ft_strlcpy(dst, src, dstsize - dst_len);
+// 	return (dst_len + ft_strlen(src));
+// }
+
 
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dst_len;
+	size_t	src_len;
 
-	dst_len = ft_strlen(dst);
-	dst += dst_len;	
+	src_len = ft_strlen(src);
 	if (dstsize == 0)
-		return (ft_strlen(src));
+		return (src_len);
+	dst_len = ft_strlen(dst);
 	if (dst_len >= dstsize)
-		return (dstsize + ft_strlen(src));
-	ft_strlcpy(dst, src, dstsize - dst_len);
-	return (dst_len + ft_strlen(src));
+		return (src_len + dstsize);
+	ft_strlcpy(dst + dst_len, src, dstsize - dst_len);
+	return (dst_len + src_len);
 }
