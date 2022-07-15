6b936dd09d73412af5d102743da951e083bbdee4
maria <winterzaq1@gmail.com>
2022-07-16 07:29:15 +0900

    update

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:20:43 by myoshika          #+#    #+#             */
-/*   Updated: 2022/07/16 07:27:10 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/07/16 07:28:28 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,16 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	needle_len;
 
 	needle_len = ft_strlen(needle);
-	if (*needle == '\0' || len == 0)
+	if (*needle == '\0')
 		return ((char *)haystack);
-	while (*haystack != '\0' && needle_len < len)
+	if (len < needle_len)
+		return (NULL);
+	len -= needle_len - 1;
+	while (*haystack != '\0' && len--)
 	{
 		if (!ft_strncmp(haystack, needle, needle_len))
 			return ((char *)haystack);
 		haystack++;
-		len--;
 	}
 	return (NULL);
 }
