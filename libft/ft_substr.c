42fd1a335b724baa18e9f2bb6efa51fc439e618f
Maria Yoshikawa <myoshika@c1r27s9.42tokyo.jp>
2022-07-19 14:07:11 +0900

    update

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 23:29:27 by myoshika          #+#    #+#             */
-/*   Updated: 2022/07/19 13:42:43 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/07/19 14:07:06 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	sub_len;
 	char	*sub;
 
-	if (s == NULL)
+	if (s == NULL || *s == '\0')
 		return (NULL);
 	sub_len = ft_strnlen(s + start, len);
-	if (sub_len <= 0)
+	if (sub_len == 0 || ft_strlen(s) <= start)
 		return (ft_strdup(""));
 	sub = (char *)malloc(sub_len + 1);
 	if (sub == NULL)
